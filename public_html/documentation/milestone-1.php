3a469a8c9310ae20764e525013c9496a6b26d927
gerald-fongwe <geraldjam2004@yahoo.com>
2015-10-31 15:25:26 -0600

    added a little bit of detail as to what I want to do in milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,5 +9,15 @@
 		<p>My web page is going to be about me, my coming to the US,
 			and the choices i have made and how those choices have
 			help me to become who i am and the choices i am making
-			now that will affect and equip me for the future.</p>
+			now that will affect and equip me for the future.
+		To let employers know that i am a person of character,
+			who would easily adapt to any work environment, always
+			willing and ready to learn, will to give the best of me
+		my best as a team member or as an individual, and will encourage
+		others colleagues.</p>
+		<p>Employers looking for adaptable workers who are ready to
+		go the extra mile to work and give their best, not easily
+			discouraged by circumstances and differences in the work
+			environment </p>
+
 	</body
