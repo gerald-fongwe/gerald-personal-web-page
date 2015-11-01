273fb80a2edbe35ef035b61d38fe433ec057ab6b
gerald-fongwe <geraldjam2004@yahoo.com>
2015-10-31 23:16:51 -0600

    added a little bit of detail as to what I want to do in milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -19,7 +19,7 @@
 		go the extra mile to work and give their best, not easily
 			discouraged by circumstances and differences in the work
 			environment. My website will be viewed by Corporate
-			employers especially those involved in customers service,
+			employers especially those involved in customer service,
 			Oil and Gas companies because I have experience in those
 			areas and possibly institutions such as schools, colleges
 			and the government.</p>
