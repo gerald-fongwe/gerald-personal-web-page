1ace4329ca17e0a29741d8fcbfb962dbc802ed63
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-07 16:46:44 -0700

    added the persona in milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,15 +8,23 @@
 		<h1>Persona, Use case and User experience</h1>
 		<h2>State Worker's Persona</h2>
 		<h3>Name: Kelly G. Bronson</h3>
-		<p><strong>Profession:</strong>Kelly is a 38 years old middle class senior developer, who works for the United
-			state Postal services(USPS).
+		<p><strong>Profession: </strong>Kelly is a 38 years old middle class senior developer, who works for the United
+			state Postal services(USPS). He is responsible for the day to day running and maintenance of the USPS
+			website in Albuquerque. He just lost  one of his web developer who just got hired by a high profile
+			organization with a good pay.</p>
+		<p><strong>Technology: </strong> He uses a Lenovo Y50 Ultra HD, Intel i7 Laptop with windows 8, an HP
+			Pavilion 550qe Desktop, Intel i5 with windows 8 and a Samsung Galaxy Note 4 Android phone connected to G4
+			Network.</p>
+		<p><strong>Attitude and Behavior</strong>
 			He is involved in the recruitment of entry level junior developers that will update and maintain the USPS
 			website in Albuquerque. He has been working as a web developer for the past eleven years and has a high level
 			of understanding in the web development field and he has a very good grasp of almost all the computer languages,
 			both front and back end languages.He is also very current with the changes happening in the IT word. Kelly is
 			looking forward to recruit a junior web developer who understands the structural differences between the
 			frontend and backend languages and can recognize, differentiate and use them to a certain level while maintaining
-			a strong level of security.</p>
+			a strong level of security.</p>looking for a replacement with a feature plan of restructuring and
+		redesigning of the website.
+
 
 
 
