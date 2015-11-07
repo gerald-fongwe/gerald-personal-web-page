5343f5bef01979cfe1709fbb6419a1dedba753f5
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-07 15:07:17 -0700

    added the persona in milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,8 +10,12 @@
 		<p>Kelly is a 38 years old middle class senior developer, who works for the United state Postal services(USPS).
 			He is involved in the recruitment of entry level junior developers that will update and maintain the USPS
 			website in Albuquerque. He has been working as a web developer for the past eleven years and has a high level
-			of understanding in the web development field. He has a very grasp of almost all the computer languages, both
-			front and back end languages. </p>
+			of understanding in the web development field and he has a very good grasp of almost all the computer languages,
+			both front and back end languages.He is also very current with the changes happening in the IT word. Kelly is
+			looking forward to recruit a junior web developer who understands the structural differences between the
+			frontend and backend languages and can recognize, differentiate and use them to a certain level while maintaining
+			a stron level of security.</p>
+
 
 
 	</body>
