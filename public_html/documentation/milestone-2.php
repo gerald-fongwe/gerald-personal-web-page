1b5184c6729eea0f0d7c6480a367bf9cfbbf1196
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-07 13:41:04 -0700

    added milestone-2 into php

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone Two</title>
+	</head>
+	<body>
+		<h1>Persona, Use case and User experience</h1>
+		<h2>Persona</h2>
+
+
+	</body>
