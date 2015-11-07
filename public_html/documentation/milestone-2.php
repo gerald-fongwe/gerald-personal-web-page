9b6e2d0c07e2a30aad912759b6ff5b1be4065e26
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-07 14:11:11 -0700

    added milestone-2 into php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,10 +6,12 @@
 	</head>
 	<body>
 		<h1>Persona, Use case and User experience</h1>
-		<h2>Persona</h2>
+		<h2>State Worker's Persona</h2>
 		<p>Kelly is a 38 years old middle class senior developer, who works for the United state Postal services(USPS).
 			He is involved in the recruitment of entry level junior developers that will update and maintain the USPS
-			website.  </p>
+			website in Albuquerque. He has been working as a web developer for the past eleven years and has a high level
+			of understanding in the web development field. He has a very grasp of almost all the computer languages, both
+			front and back end languages. </p>
 
 
 	</body>
