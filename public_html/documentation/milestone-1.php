e5acf34b6b178f467793c5f115ba2f1ad4253c3d
gerald-fongwe <geraldjam2004@yahoo.com>
2015-12-10 17:10:50 -0700

    added image for the personal-web-page wire frame.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,17 +6,19 @@
 	</head>
 	<body>
 		<h1>The idea behind my web page</h1>
+
 		<p>My web page is going to be about me, my coming to the US,
 			and the choices i have made and how those choices have
 			help me to become who i am and the choices i am making
 			now that will affect and equip me for the future.
-		It will be to let employers know that i am a person of character,
+			It will be to let employers know that i am a person of character,
 			who can easily adapt to any work environments, always
 			willing and ready to learn, willing to give the best of me
-		 as a team member or as an individual, reliable and will encourage
-		other colleagues as we work together.</p>
+			as a team member or as an individual, reliable and will encourage
+			other colleagues as we work together.</p>
+
 		<p>Employers looking for adaptable workers who are ready to
-		go the extra mile to work and give their best, not easily
+			go the extra mile to work and give their best, not easily
 			discouraged by circumstances and differences in the work
 			environment. My website will be viewed by Corporate
 			employers especially those involved in customer service,
@@ -24,4 +26,4 @@
 			areas and possibly institutions such as schools, colleges
 			and the government.</p>
 	</body>
-		</html>
+</html>
---
+++ public_html/documentation/milestone-4.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				rel="stylesheet"/>
+		<link rel="stylesheet" href="../css/style.css" type="text/css"/>
+		<!-- <link rel="stylesheet" href="/css/master.css"> -->
+
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>Milestone-3</title>
+	</head>
+	<header>
+		<div class="container">
+			<a class="logo" href="#">Building Bricks</a>
+
+			<div class="nav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">About Me</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
